42c93a1484ecca0bb5dcd26ff03697c2b9e2c8f1
stephenavocado <stephenwalter@chicagobooth.edu>
2022-11-12 19:47:13 +0000

    solution video changes

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,11 +2,17 @@ import React from "react";
 import { range } from "../../utils";
 
 function Guess({ guess }) {
+
   return (
     <p className="guess">
       {
         range(5).map((num, index) => (
-          <span key={index} className={`cell ${guess && guess[num].status }`}>{guess ? guess[num].letter : undefined}</span>
+          <span 
+            key={index} 
+            className={`cell ${guess && guess[num].status }`}
+          >
+            {guess ? guess[num].letter : undefined}
+          </span>
         ))
       }
     </p>
