2e2466556b49aca3cc1c2bdb34fd9b6995524743
stephenavocado <stephenwalter@chicagobooth.edu>
2022-11-12 19:37:30 +0000

    game-logic SW attempt

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -6,7 +6,7 @@ function Guess({ guess }) {
     <p className="guess">
       {
         range(5).map((num, index) => (
-          <span key={index} className="cell">{guess ? guess[num] : undefined}</span>
+          <span key={index} className={`cell ${guess && guess[num].status }`}>{guess ? guess[num].letter : undefined}</span>
         ))
       }
     </p>
